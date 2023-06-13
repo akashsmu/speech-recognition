cfdf9d734ea110e6e320b757685aacfce6f05115
Akash Santebennur Matada Umapathi <45820053+akashsmu@users.noreply.github.com>
2023-06-13 18:44:01 -0400

    added recording feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AudioRecorder from "../src/AudioRecorder";
 
-function App() {
+const App = () => {
+  let [recordOption, setRecordOption] = useState("video");
+
+  const toggleRecordOption = (type) => {
+    return () => {
+      setRecordOption(type);
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Audio Recorder</h1>
+      {/* <div className="button-flex">
+        <button onClick={toggleRecordOption("audio")}>Record Audio</button>
+      </div> */}
+      <div>
+        <AudioRecorder />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
